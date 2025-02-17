147e1fad3ac2b326336ac0c9a5d043c26cc5365d
Snake-EyE-12 <chase.guymon@q.com>
2025-02-16 20:30:26 -0700

    Illion Begin

--- Assets/_Project/Systems/IllionSystem/Illion.cs
+++ Assets/_Project/Systems/IllionSystem/Illion.cs
@@ -4,8 +4,59 @@ namespace Cobra.Utilities
 {
     public class Illion
     {
-        private int sectionValue;
-        public int Value => sectionValue;
+        private class Section
+        {
+            private int tri;
+            private Section next;
+            public int Value => GetValue();
+
+            private int GetValue() // use keyword checked
+            {
+                int thou = tri;
+                if (next != null)
+                {
+                    thou += next.Value * 1000;
+                }
+                return thou;
+            }
+
+            public void Add(int value)
+            {
+                //make sure this section doesn't get too big
+                //else
+                tri += value;
+                int extra = tri / 1000;
+                tri %= 1000;
+                Propagate(extra);
+            }
+
+            private void Propagate(int value)
+            {
+                if (value > 0)
+                {
+                    if (next == null)
+                    {
+                        next = new Section(value);
+                        return;
+                    }
+                    next.Add(value);
+                }
+            }
+
+            public Section(int value)
+            {
+                // make sure not too big
+                tri = value;
+            }
+        }
+        
+        private Section section;
+        public int Value => GetValue();
+
+        private int GetValue()
+        {
+            return section.Value;
+        }
 
         public static Illion operator +(Illion self, int value)
         {
@@ -20,7 +71,7 @@ namespace Cobra.Utilities
 
         private void Add(int value)
         {
-
+            section.Add(value);
         }
 
         private void Subtract(int value)
@@ -29,6 +80,12 @@ namespace Cobra.Utilities
         }
 
 
+        public Illion(int value)
+        {
+            section = new Section(value);
+        }
+        public static implicit operator int(Illion illion) => illion.Value;
+        public static implicit operator Illion(int v) => new Illion(v);
         public override string ToString()
         {
             return "Illion";
