74c1f90fc8e5cae6a9865d51024d9f0fb4c67508
Chase Guymon <chase.guymon@q.com>
2025-08-01 00:48:14 -0600

    Pause Game Scene

--- Assets/_Project/Systems/MenuSystem/PauseController.cs
+++ Assets/_Project/Systems/MenuSystem/PauseController.cs
@@ -1,14 +1,21 @@
+using NaughtyAttributes;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Cobra.GUI
 {
     public class PauseController : MonoBehaviour
     {
         private float previousTimeScale;
+        [SerializeField] private InputActionAsset inputSystem;
+        [SerializeField] private PlayerInput playerInput;
+        [SerializeField] private string actionMapToggle;
         public void Pause()
         {
             AudioListener.pause = true;
             previousTimeScale = Time.timeScale;
+            //playerInput.actions.Disable();
+            
             Time.timeScale = 0;
         }
 
@@ -16,6 +23,12 @@ namespace Cobra.GUI
         {
             AudioListener.pause = false;
             Time.timeScale = previousTimeScale;
+        }
+
+        [Button]
+        public void disableActions()
+        {
+            inputSystem.FindActionMap(actionMapToggle).Disable();
         }
     }
 }
